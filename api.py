44d13d10663baf406362c42f118b247c44f51483
Tiagodnzz <tiagodnzz@gmail.com>
2022-09-26 20:48:50 -0300

    subindo projeto

---
+++ api.py
@@ -0,0 +1,52 @@
+import os
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app = Flask(__name__)
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'senha'
+app.config['MYSQL_DATABASE_DB'] = 'banco'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
+mysql.init_app(app)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/gravar', methods=['POST','GET'])
+def gravar():
+  nome = request.form['nome']
+  preco = request.form['preco']
+  categoria = request.form['categoria']
+  if nome and preco and categoria:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute('insert into tbl_prod (prod_nome, prod_preco, prod_categoria) VALUES (%s, %s, %s)', (nome, preco, categoria))
+    conn.commit()
+  return render_template('index.html')
+
+
+@app.route('/listar', methods=['POST','GET'])
+def listar():
+  conn = mysql.connect()
+  cursor = conn.cursor()
+  cursor.execute('select uprod_nome, prod_preco, prod_categoria from tbl_prod')
+  data = cursor.fetchall()
+  conn.commit()
+  return render_template('lista.html', datas=data)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5008))
+    app.run(host='0.0.0.0', port=port)
+
+
+    # SQL
+
+    # create schema banco;
+
+    # use banco;
+
+    # CREATE TABLE tbl_prod ( prod_id BIGINT NOT NULL AUTO_INCREMENT, prod_nome VARCHAR(45) NULL, prod_preco VARCHAR(45) NULL, prod_categoria VARCHAR(45) NULL, PRIMARY KEY (user_id));
